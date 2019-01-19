4eeaeb185229825292b6ae655c9f46da00c51679
John Cinq-Mars <jcinqmars2000@yahoo.com>
2019-01-19 12:00:31 -0500

    Initial commit

---
+++ src/main/java/com/responsible_nrg/NounColdServerApplication.java
@@ -0,0 +1,14 @@
+package com.responsible_nrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NounColdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NounColdServerApplication.class, args);
+	}
+}
